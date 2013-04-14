1f0011600c5394df5271bc9ee1d007b1e4bd9990
schmitt2048 <aschmitt@panix.com>
2013-04-14 10:44:11 -0400

    Commit 42: 1. Cleaned up typos in language files  2. Removed some parameters that didn't work like selecting specific events  3. Added left join to join in events that have no address info the the street address field or other address parameter is listed.  4. Corrected order of names of the week in translation files.  5. Rewrote some of the code to use JDatabaseQuery and tested parameters related to event selection.  6. Made some small tweaks to default install parameters.  7. Remove some dead code that was not needed for linking to registration pages.  8. Fixed Links to w3 site for color names in back end.

--- helper.php
+++ helper.php
@@ -7,182 +7,128 @@ class modCiviCRMFullCalendarHelper
     {
         jimport('joomla.application.module.helper');
 
-        $db =& JFactory::getDBO();
-        $mode = trim($params->get('mode'));
-        $result = null;
-        $link = trim($params->get('link'));
-        $multievent = $params->get('multievent');
-        $tid = trim($params->get('tid'));
-        $startdate = trim($params->get('startdate'));
-        $privacy = trim($params->get('privacy'));
-        $enddate = trim($params->get('enddate'));
-        $pastevents = trim($params->get('pastevents'));
-        $isonline = " AND is_online_registration = 1 ";
+         
+		// let's use the JDatabaseQuery class for assembly
+		// see more here:
+		//   http://www.theartofjoomla.com/9-developer/135-database-upgrades-in-joomla-16.html
+		// and
+		//   http://docs.joomla.org/API16:JDatabaseQuery/select
+        jimport( 'joomla.database.databasequery' );
 
-        
-        
-
-        //retrieve all custom data tables impacting events
-        $customdatapresent = 1; //assume data unless none found below
-        $query_customevent = 
-        	"SELECT table_name ".
-        	"FROM civicrm_custom_group ".
-        	"WHERE extends = 'Event' ";
-        
-        $db->setQuery($query_customevent);
-        $result_customevent = $db->loadObjectList();
-        
-        if ($db->getErrorNum()) {
-            $customdatapresent = 0;
-        }
-        //print_r($result_customevent);
-        //echo '<br />';
-
-        
-        
-        $select = "SELECT ";
-        $from = " FROM " ;
-        $where = " WHERE " ; 
-        $orderby = " ORDER BY ";
-
-         //set core SELECT and FROM clauses based on presence of custom fields
-        $customdatapresent = 0 ; if ($customdatapresent == 0) { //no custom data
-            $select .= "e.title, e.id AS eventID, e.start_date, e.end_date, ".
-            			"e.event_type_id, e.summary, e.is_active, ".
-            			"e.is_public, e.is_online_registration " ;
-            $from .= "civicrm_event e ";
-        } elseif ($customdatapresent == 1) { //custom data present
-
-            //for each custom event custom data table, build SELECT and FROM clause
-            //$arraycount = 0;
-            $select .= "e.civicrm_event.id AS e.eventID, e.* ";
-            $from .= "civicrm_event e";
-
-            foreach ($result_customevent as $key => $value) { //recurse through the custom tables and build sql
-                foreach ($value as $tablename) {
-                    $select .= ', ' . $tablename . '.*';
-                    $from .= ' LEFT OUTER JOIN ' . $tablename . ' ON (' . $tablename . '.entity_id = civicrm_event.id)';
-                }
-            }
-        } 
-        
-        
-        //set core WHERE clause
-        $where .= 'e.is_active = 1 AND e.is_template != 1 ';
-
-
-        $currentdate = date("Y-m-d");
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select("e.title, e.id AS eventID, e.start_date, e.end_date, ".
+        		"e.event_type_id, e.summary, e.is_active, ".
+        		"e.is_public, e.is_online_registration");
+        $query->from('civicrm_event e') ;
+        $query->where('e.is_active = 1 AND e.is_template != 1');
+        $query->order('e.start_date ASC');
 
         //disable past events setting
-        if ($pastevents){
-            //only view events that end on or after current date, or where no end_date is defined
-            $wheredaterange = " AND ( e.end_date >= '" . $currentdate . "' OR e.end_date IS NULL OR e.end_date = '' ) ";
-            //only view events that start on or after current date
-            $wheredaterange .= " AND e.start_date >= '" . $currentdate . "'";
-        }
-
-        //determine privacy setting
-        switch ($privacy) {
-            case(0):
-            {
-            	// both public or private
-                $privacy = "";
-                break;
-            }
-            case(1):
-            {
-            	// public only
-                $privacy = " AND e.is_public = 1 ";
-                break;
-            }
-            case(2):
-            {
-            	// private only
-                $privacy = " AND e.is_public = 0 ";
-                break;
-            }
+        $showpastevents =  trim($params->get('showpastevents'));
+        $currentdate = date("Y-m-d");
+        if (! $showpastevents){
+        	// OK, so hide past events, or events that are not in the future ....
+        	//only view events that end on or after current date, or where no end_date is defined
+        	$query->where("( e.end_date >= '" . $currentdate . "' OR e.end_date IS NULL OR e.end_date = '') ");
+        	// OR e.end_date = \'\'");
+        	//only view events that start on or after current date
+        	$query->where("e.start_date >= '" . $currentdate ."'");
         }
 
         
-        //determine link type (info or reg). if reg, make sure events have online reg setup
-        if ($link == "1") {
-            $where .= $isonline;
+        
+        
+        //determine privacy setting
+        $privacy = trim($params->get('privacy'));
+        switch ($privacy) {
+        	case(0):
+        		{
+        			// both public or private
+        			break;
+        		}
+        	case(1):
+        		{
+        			// public only
+        			$query->where("e.is_public = 1 ");
+        			break;
+        		}
+        	case(2):
+        		{
+        			// private only
+        			$query->where("e.is_public = 0 ");
+        			break;
+        		}
         }
-
-        //determine display mode, affect where clause where appropriate
+        
+        
+        //determine display mode
+        $mode = trim($params->get('mode'));
         switch ($mode) {
-            case(0): //default mode
-            {
-            	break;
-            }
+        	case(0): //default mode
+        		{
+        			break;
+        		}
 
-            case(1): //date range mode
-            {
-                //Rewrite date format
-                if ($startdate) $startdate = date('Y-m-d', strtotime($startdate));
-                if ($enddate) {
-                    //Add 1 day to end date to fix date range criteria
-                    $enddate = strtotime('+1 day', strtotime($enddate));
-                    $enddate = date('Y-m-d', $enddate);
-                }
+        	case(1): //date range mode
+        		{
+        			$startdate = trim($params->get('startdate'));
+    		    	$enddate = trim($params->get('enddate'));
 
-                if ($startdate == "" OR $startdate == "Select") //no start date selected, throw error
-                {
-                    JError::raiseWarning(500, "No start date selected for CiviEvent module.");
-                } elseif ($enddate == "" OR $enddate == "Select") //Open end date parameter
-                {
-                    $wheredaterange = " AND e.start_date >= '" . $startdate . "'";
-                    
-                } else //both start and end date parameter, replace default end date range
-                {
-                    //v2 BOTH start/end date measured by event start date in order to make month-wrap ranges ruled by start
-                    $wheredaterange = " AND e.start_date >= '" . $startdate . "'" . " AND e.start_date < '" . $enddate . "'";
-                    
-                }
-                break;
-            }
 
-            case(2): //custom select mode
-            {
-                $where .= ((count($multievent) > 1) ? ' AND ( e.id=' . implode(' OR e.id= ', $multievent) . ' ) ' : ' AND e.id=' . $multievent);
-                break;
-            }
+    		    	//Rewrite date format
+        			if ($startdate) {
+        				$startdate_formatted = date('Y-m-d', strtotime($startdate));
+        			}
+    		    	if ($enddate) {
+        				$enddate_formatted = date('Y-m-d', strtotime($enddate));
+    		    	}
 
-            case(3): //event type mode
-            {
-                $where .= ' AND e.event_type_id = ' . $tid;
-                break;
-            }
+    		    	/*
+    		    	echo "\$startdate: ".$startdate."<BR />" ; // debug
+    		    	echo "\$enddate: ".$enddate."<BR />" ; // debug
+    		    	echo "\$startdate_formatted: ".$startdate_formatted."<BR />" ; // debug
+        			echo "\$enddate_formattted: ".$enddate_formatted."<BR />" ; // debug
+					*/
+    		    	
+        			if ($startdate == "" OR $startdate == "Select") //no start date selected, throw error
+        			{
+        				JError::raiseWarning(500, "No start date selected for mod_civicrmfullcalendar module.");
+        			} elseif ($enddate == "" OR $enddate == "Select") //Open end date parameter
+        			{
+        				$query->where("e.start_date >= '" . $startdate_formatted . "'");
+
+        			} else //both start and end date parameter, replace default end date range
+        			{
+        				$query->where("e.start_date >= '" . $startdate_formatted . "' AND " .
+        						"e.end_date <= '" . $enddate_formatted . "'");
+        			}
+        			break;
+        		}
+
+        	case(2): //event type mode
+        		{
+        			$tid = trim($params->get('tid'));
+        			$query->where("e.event_type_id =".$tid);
+        			break;
+        		}
         } //close mode switch
 
         
-        $orderby .= '1'; 
-
-        // do we even need the location?
-        if ( 
-              (stristr(trim($params->get('fieldtemplate')), "{event_street_address}") != FALSE) || 
-              (stristr(trim($params->get('fieldtemplate')), "{event_supplemental_address_1}") != FALSE) ||
-              (stristr(trim($params->get('fieldtemplate')), "{event_supplemental_address_2}") != FALSE) 
+        
+        // do we even need the location?  ... don't forget, some events MIGHT NOT HAVE A LOCATION!
+        if (
+        		(stristr(trim($params->get('fieldtemplate')), "{event_street_address}") != FALSE) ||
+        		(stristr(trim($params->get('fieldtemplate')), "{event_supplemental_address_1}") != FALSE) ||
+        		(stristr(trim($params->get('fieldtemplate')), "{event_supplemental_address_2}") != FALSE)
         ) {
         	// get the event location data
-        	$select .= ", a.street_address, a.supplemental_address_1, a.supplemental_address_2 ";
-        	$from   .= ", civicrm_address a, civicrm_loc_block lb ";
-        	$where  .= " AND e.loc_block_id = lb.id AND a.id = lb.id " ;
+        	$query->select( "a.street_address, a.supplemental_address_1, a.supplemental_address_2 ");
+        	$query->leftJoin('civicrm_loc_block lb ON e.loc_block_id = lb.id')->leftJoin('civicrm_address a ON a.id = lb.id');
         }
 
+        // DEBUG echo $query ;  // DEBUG
         
-        // public, private or both???
-        $where .= $privacy ; 
-         
-       	
-       	
-        //build query statement
-        $query = $select . ' ';
-        $query .= $from . ' ';
-        $query .= $where . $wheredaterange ; 
-        $query .= $orderby . '  ' ;
-        // DEBUG echo $query ;  // DEBUG 
-
         //run $query;
         $db->setQuery($query);
         $result = $db->loadObjectList();
@@ -190,6 +136,7 @@ class modCiviCRMFullCalendarHelper
         if ($db->getErrorNum()) {
             JError::raiseWarning(500, "No events meet the selected criteria.");
         }
+
 
         return $result;
 
@@ -212,6 +159,7 @@ class modCiviCRMFullCalendarHelper
         $displayParams['color4'] = trim($params->get('color4'));
         $displayParams['color5'] = trim($params->get('color5'));
         $displayParams['color6'] = trim($params->get('color6'));
+        $displayParams['showpastevents'] = trim($params->get('showpastevents'));
         $displayParams['colorpicker'] = trim($params->get('colorpicker'));
         $displayParams['legendpicker'] = trim($params->get('legendpicker'));
         $displayParams['customcolors'] = trim($params->get('customcolors'));
