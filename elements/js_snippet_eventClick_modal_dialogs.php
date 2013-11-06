599f78cd79b18649c85649eb501d82ca1bd7b201
schmitt2048 <aschmitt@panix.com>
2013-11-06 00:05:25 -0500

    60: fixes bug fix introducted in 59.

--- elements/js_snippet_eventClick_modal_dialogs.php
+++ elements/js_snippet_eventClick_modal_dialogs.php
@@ -7,8 +7,10 @@ $js_snippet_eventClick_modal_dialogs = <<<'ECMD'
 	eventClick: function(event) {
     	    if (event.url) {
 				//$cfcj('a[id$="aref_mod_civicrm_fullcalendar_dialog"]').attr("href", event.url   );
-				$('a[id$="aref_mod_civicrm_fullcalendar_dialog"]').attr("href", event.url   );		
-				$('aref_mod_civicrm_fullcalendar_dialog' ).click() ;
+				cfcj('a[id$="aref_mod_civicrm_fullcalendar_dialog"]').attr("href", event.url   );		
+
+		        // this is correct with just $() 
+		        $('aref_mod_civicrm_fullcalendar_dialog' ).click() ;
 				return false;
 	        } // if
 	} // eventClick: function(event)
