15bc588b2183526a5cb16995924fd5717c2afc82
schmitt2048 <aschmitt@panix.com>
2013-11-16 09:55:21 -0500

    Commit 63: reverts back to commit 58.  has no conflict, does not use the dollar sign variable, spinner code is there but not called.

--- elements/js_snippet_eventClick_modal_dialogs.php
+++ elements/js_snippet_eventClick_modal_dialogs.php
@@ -7,12 +7,8 @@ $js_snippet_eventClick_modal_dialogs = <<<'ECMD'
 	eventClick: function(event) {
     	    if (event.url) {
 				//$cfcj('a[id$="aref_mod_civicrm_fullcalendar_dialog"]').attr("href", event.url   );
-				$('a[id$="aref_mod_civicrm_fullcalendar_dialog"]').attr("href", event.url   );		
-
-		        // this is correct with just $() 
-		        // RESTORE $('aref_mod_civicrm_fullcalendar_dialog' ).click() ;
-	        	//$('a[id$="aref_mod_civicrm_fullcalendar_dialog.modal"]').click();
-		//  alert( $('aref_mod_civicrm_fullcalendar_dialog' ).click()  );
+				cfcj('a[id$="aref_mod_civicrm_fullcalendar_dialog"]').attr("href", event.url   );		
+				$('aref_mod_civicrm_fullcalendar_dialog' ).click() ;
 				return false;
 	        } // if
 	} // eventClick: function(event)
